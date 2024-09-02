bdd73d4fadb008dd03f7a2c70a4e166fb27d39d2
raiden11 <gupta.ayush11786@gmail.com>
2024-09-02 22:37:16 +0530

    Adding admin panel backend and url

--- backend/mytms/admin.py
+++ backend/mytms/admin.py
@@ -1,3 +1,15 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Campaign, Member, Task
+
+@admin.register(Campaign)
+class CampaignAdmin(admin.ModelAdmin):
+    list_display = ['name', 'created_at', 'updated_at']
+
+@admin.register(Member)
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ['email', 'full_name', 'role', 'created_at', 'updated_at']
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['name', 'status', 'score', 'campaign', 'trainer', 'lead', 'created_at', 'updated_at']
